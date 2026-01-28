0752e6f0f66ea09e10d116143e77a41dbba530fa
Jeff Gonzalez <jeff@hypertheory.com>
2026-01-28 10:43:22 -0500

    before

--- src/app/areas/labs/jefflabs-landing/jefflabs-landing/internal/widgets/clock.ts
+++ src/app/areas/labs/jefflabs-landing/jefflabs-landing/internal/widgets/clock.ts
@@ -11,45 +11,46 @@ export type Task = {
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgIcon],
   template: `
-    <div class="flex flex-row items-center">
-      @if (inTaskRecording()) {
-        <button class="btn btn-xs btn-circle btn-success mr-2" (click)="finishTask()">
-          <ng-icon name="lucideX" size="20"></ng-icon>
-        </button>
-        <button class="btn btn-xs btn-circle btn-error mr-2" (click)="cancelTask()">
-          <ng-icon name="lucideCircleSlash" size="20"></ng-icon>
-        </button>
-      } @else {
-        <button (click)="startTask()" class="btn btn-xs btn-circle btn-success mr-2">
-          <ng-icon name="lucidePlay" size="20"></ng-icon>
-        </button>
-      }
-
-      <span
-        class="countdown font-mono text-green-500 text-2xl "
-        [class.animate-pulse]="inTaskRecording()"
-      >
+    <div class="flex flex-col gap-2">
+      <div class="flex flex-row items-center">
+        @if (inTaskRecording()) {
+          <button class="btn btn-xs btn-circle btn-success mr-2" (click)="finishTask()">
+            <ng-icon name="lucideX" size="20"></ng-icon>
+          </button>
+          <button class="btn btn-xs btn-circle btn-error mr-2" (click)="cancelTask()">
+            <ng-icon name="lucideCircleSlash" size="20"></ng-icon>
+          </button>
+        } @else {
+          <button (click)="startTask()" class="btn btn-xs btn-circle btn-success mr-2">
+            <ng-icon name="lucidePlay" size="20"></ng-icon>
+          </button>
+        }
         <span
-          style="--value:{{ currentTime().hours }};"
-          aria-live="polite"
-          aria-label="{{ currentTime().hours }}"
-          >{{ currentTime().hours }}</span
+          class="countdown font-mono text-green-500 text-2xl "
+          [class.animate-pulse]="inTaskRecording()"
         >
-        :
-        <span
-          style="--value:{{ currentTime().minutes }}; --digits: 2;"
-          aria-live="polite"
-          aria-label="{{ currentTime().minutes }}"
-          >{{ currentTime().minutes }}</span
-        >
-        :
-        <span
-          style="--value:{{ currentTime().seconds }}; --digits: 2;"
-          aria-live="polite"
-          aria-label="{{ currentTime().seconds }}"
-          >{{ currentTime().seconds }}</span
-        >
-      </span>
+          <span
+            style="--value:{{ currentTime().hours }};"
+            aria-live="polite"
+            aria-label="{{ currentTime().hours }}"
+            >{{ currentTime().hours }}</span
+          >
+          :
+          <span
+            style="--value:{{ currentTime().minutes }}; --digits: 2;"
+            aria-live="polite"
+            aria-label="{{ currentTime().minutes }}"
+            >{{ currentTime().minutes }}</span
+          >
+          :
+          <span
+            style="--value:{{ currentTime().seconds }}; --digits: 2;"
+            aria-live="polite"
+            aria-label="{{ currentTime().seconds }}"
+            >{{ currentTime().seconds }}</span
+          >
+        </span>
+      </div>
     </div>
   `,
   styles: ``,
